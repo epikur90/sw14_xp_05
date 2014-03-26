bb6425b364622b1c9e0830cb174122aa11b730c6
Daniel Moser <d.moser@student.tugraz.at>
2014-03-26 13:42:42 +0100

    fixed project

--- Pinkee/gen/com/laforge/pinkee/R.java
+++ Pinkee/gen/com/laforge/pinkee/R.java
@@ -1097,8 +1097,7 @@ containing a value of this type.
         public static final int abc_search_dropdown_item_icons_2line=0x7f030015;
         public static final int abc_search_view=0x7f030016;
         public static final int activity_main=0x7f030017;
-        public static final int fragment_main=0x7f030018;
-        public static final int support_simple_spinner_dropdown_item=0x7f030019;
+        public static final int support_simple_spinner_dropdown_item=0x7f030018;
     }
     public static final class menu {
         public static final int main=0x7f0c0000;
