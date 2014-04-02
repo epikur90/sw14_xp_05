0a99b544c286857c555f100fd557816a8328764e
Daniel Moser <d.moser@student.tugraz.at>
2014-04-02 13:34:06 +0200

    fixo

--- PinKee/src/com/sw14_xp_05/pinkee/Common.java
+++ PinKee/src/com/sw14_xp_05/pinkee/Common.java
@@ -3,6 +3,13 @@ package com.sw14_xp_05.pinkee;
 import java.util.ArrayList;
 import java.util.List;
 
+import android.accounts.Account;
+import android.accounts.AccountManager;
+import android.app.Application;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+import android.util.Patterns;
+
 
 public class Common extends Application {   
 	 
--- PinKee/src/com/sw14_xp_05/pinkee/MainActivity.java
+++ PinKee/src/com/sw14_xp_05/pinkee/MainActivity.java
@@ -1,15 +1,14 @@
 package com.sw14_xp_05.pinkee;
 
-import java.awt.Menu;
-import java.awt.MenuItem;
-
-import javax.swing.text.View;
-
-import android.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.ActionBarActivity;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
 import android.view.ViewGroup;
+import com.sw14_xp_05.pinkee.R;
 
 public class MainActivity extends ActionBarActivity {
 
@@ -24,8 +23,6 @@ public class MainActivity extends ActionBarActivity {
 		}
 	}
 
-	
-	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 
