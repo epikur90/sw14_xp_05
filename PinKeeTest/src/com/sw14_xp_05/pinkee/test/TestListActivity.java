7426324326e924f574146f4353e0f7a27507c5f2
poberlohr <patrick.oberlohr@gmx.at>
2014-04-09 08:50:47 +0200

    Second test added

---
+++ PinKeeTest/src/com/sw14_xp_05/pinkee/test/TestListActivity.java
@@ -0,0 +1,26 @@
+package com.sw14_xp_05.pinkee.test;
+
+import android.test.ActivityInstrumentationTestCase2;
+
+import com.robotium.solo.Solo;
+import com.sw14_xp_05.pinkee.ListActivity;
+
+public class TestListActivity extends ActivityInstrumentationTestCase2<ListActivity> {
+	private Solo mySolo;
+	public TestListActivity() {
+		super(ListActivity.class);
+	}
+	
+	protected void setUp() throws Exception {
+		super.setUp();
+		mySolo = new Solo(getInstrumentation(), getActivity());
+	}
+
+	protected void tearDown() throws Exception {
+		super.tearDown();
+	}
+	
+	public void testlickChatListElement() {
+		mySolo.clickInList(0);
+	}
+}
