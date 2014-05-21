fc8ec7c3c7d132896b45eccf4e624e2ec5bfde6e
Michael Scheucher <michischeucher@gmail.com>
2014-05-21 11:07:43 +0000

    contacts added all

--- app/src/main/java/com/sw14_xp_05/pinkee/Contact.java
+++ app/src/main/java/com/sw14_xp_05/pinkee/Contact.java
@@ -4,4 +4,57 @@ package com.sw14_xp_05.pinkee;
  * Created by michael on 21.05.14.
  */
 public class Contact {
+
+    private String id;
+    private String forename;
+    private String name;
+    private String email;
+    private String picture_link;
+
+    public String getFullName() {
+        return forename + " " + name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getForename() {
+        return forename;
+    }
+
+    public void setForename(String forename) {
+        this.forename = forename;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPicture_link() {
+        return picture_link;
+    }
+
+    public void setPicture_link(String picture_link) {
+        this.picture_link = picture_link;
+    }
+
+
+
 }
--- app/src/main/java/com/sw14_xp_05/pinkee/ContactsActivity.java
+++ app/src/main/java/com/sw14_xp_05/pinkee/ContactsActivity.java
@@ -1,24 +1,92 @@
 package com.sw14_xp_05.pinkee;
 
-import android.support.v7.app.ActionBarActivity;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Vector;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.ActionBarActivity;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
+import android.util.Log;
 
 
-public class ContactsActivity extends ActionBarActivity {
+import com.google.android.gms.common.AccountPicker;
 
+import java.util.ArrayList;
+
+
+public class ContactsActivity extends ActionBarActivity implements OnClickListener {
+
+    private ArrayAdapter<String> adapter;
+    private ArrayList<String> valueList;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_contacts);
-    }
+        setContentView(R.layout.activity_list);
 
+        Common utils = new Common();
+
+
+        if (!utils.isLoggenIn()) {
+            Intent intent = AccountPicker.newChooseAccountIntent(null, null,
+                    new String[]{"com.google"}, false, null, null, null, null);
+            startActivityForResult(intent, 2);
+
+
+        }
+
+        Vector<String> names = new Vector<String>();
+        names.add("Mickey Mouse");
+        names.add("Veronika");
+        names.add("Nore Ply");
+
+        // Dummy contacts
+        ArrayList<String> valueList = new ArrayList<String>();
+        for (int i = 0; i < 11; i++) {
+            valueList.add(names.elementAt(i));
+        }
+
+        adapter = new ArrayAdapter<String>(getApplicationContext(),
+                R.layout.contact_list_item, valueList);
+
+        final ListView lv = (ListView) findViewById(R.id.listView);
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view,
+                                    int position, long id) {
+
+                System.out.println(view.getClass().getName());
+                ChatActivity chatWindow = new ChatActivity();
+                Intent newActivity0 = new Intent(ListActivity.this, ChatActivity.class);
+                startActivity(newActivity0);
+
+            }
+
+        });
+
+        lv.setAdapter(adapter);
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
+
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.contacts, menu);
+        getMenuInflater().inflate(R.menu.list, menu);
         return true;
     }
 
@@ -31,6 +99,78 @@ public class ContactsActivity extends ActionBarActivity {
         if (id == R.id.action_settings) {
             return true;
         }
+        if (id == R.id.action_add_contact) {
+
+            // Set an EditText view to get user input
+            final EditText emailEditText = new EditText(this);
+
+            final AlertDialog dialog = new AlertDialog.Builder(this)
+                    .setTitle(R.string.action_add_contact)
+                    .setNegativeButton("Cancel", null)
+                    .setPositiveButton("Ok", null)
+                    .setMessage(R.string.action_add_contact_message)
+                    .setView(emailEditText)
+                    .create();
+            dialog.setOnShowListener(new DialogInterface.OnShowListener() {
+
+                @Override
+                public void onShow(DialogInterface arg0) {
+
+                    Button okButton = dialog.getButton(AlertDialog.BUTTON_POSITIVE);
+                    okButton.setOnClickListener(new View.OnClickListener() {
+
+                        @Override
+                        public void onClick(View arg0) {
+
+                            if (!android.util.Patterns.EMAIL_ADDRESS.matcher(emailEditText.getText().toString()).matches()) {
+                                Toast.makeText(getBaseContext(), R.string.action_add_contact_invalid_email, Toast.LENGTH_LONG).show();
+                            }
+                            else{
+                                dialog.dismiss();
+                                valueList.add(emailEditText.getText().toString());
+                                adapter.notifyDataSetChanged();
+                            }
+
+                        }
+                    });
+                }
+            });
+            dialog.show();
+
+            //TODO
+
+
+            return true;
+        }
+        if (id == R.id.action_show_contacts) {
+            Log.i("ListActivity", "action_show_contacts clicked");
+
+            return true;
+        }
+
         return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * A placeholder fragment containing a simple view.
+     */
+    public static class PlaceholderFragment extends Fragment {
+
+        public PlaceholderFragment() {
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_main, container,
+                    false);
+            return rootView;
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+
+
     }
 }
