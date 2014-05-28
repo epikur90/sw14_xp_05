31d9cfaab46cb9eeeb9d0676bd51916cfb573bbc
stefan3010 <stefan.maggale@student.tugraz.at>
2014-05-28 09:17:59 +0200

    Signed-off-by: stefan3010 <stefan.maggale@student.tugraz.at>

--- app/src/main/java/com/sw14_xp_05/pinkee/ChatActivity.java
+++ app/src/main/java/com/sw14_xp_05/pinkee/ChatActivity.java
@@ -1,5 +1,8 @@
 package com.sw14_xp_05.pinkee;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.ActionBarActivity;
@@ -19,6 +22,10 @@ public class ChatActivity extends ActionBarActivity {
 	private EditText textFieldMessage;
 	private MessageList messageList;
     private Contact contact;
+
+    public static final String MyPreferences = "MyPrefs";
+    public static final String Mycolor = "Mycolor";
+    public static final String Mytheme = "Mytheme";
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -89,4 +96,31 @@ public class ChatActivity extends ActionBarActivity {
 			return rootView;
 		}
 	}
+
+    protected void onResume()
+    {
+        super.onResume();
+
+        final SharedPreferences sharedpreferences = getSharedPreferences(MyPreferences, Context.MODE_PRIVATE);
+        String Colorchange = "#FFFFFF";
+        Integer Themechange = 0;
+
+        View background = getWindow().getDecorView();
+
+        if(sharedpreferences.contains(Mycolor))
+        {
+            Colorchange = sharedpreferences.getString(Mycolor, "");
+
+            background.setBackgroundColor(Color.parseColor(Colorchange));
+        }
+
+        if(sharedpreferences.contains(Mytheme))
+        {
+            Themechange = sharedpreferences.getInt(Mytheme,0);
+
+            background.setBackgroundResource(Themechange);
+        }
+
+
+    }
 }
--- app/src/main/java/com/sw14_xp_05/pinkee/ListActivity.java
+++ app/src/main/java/com/sw14_xp_05/pinkee/ListActivity.java
@@ -4,8 +4,11 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Vector;
 import android.app.AlertDialog;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.ActionBarActivity;
@@ -30,6 +33,10 @@ import java.util.ArrayList;
 
 
 public class ListActivity extends ActionBarActivity implements OnClickListener {
+
+    public static final String MyPreferences = "MyPrefs";
+    public static final String Mycolor = "Mycolor";
+    public static final String Mytheme = "Mytheme";
 
   private ArrayAdapter<String> adapter;
   private ArrayList<String> valueList;
@@ -185,4 +192,31 @@ public class ListActivity extends ActionBarActivity implements OnClickListener {
 
 
   }
+
+    protected void onResume()
+    {
+        super.onResume();
+
+        final SharedPreferences sharedpreferences = getSharedPreferences(MyPreferences, Context.MODE_PRIVATE);
+        String Colorchange = "#FFFFFF";
+        Integer Themechange = 0;
+
+        View background = getWindow().getDecorView();
+
+        if(sharedpreferences.contains(Mycolor))
+        {
+            Colorchange = sharedpreferences.getString(Mycolor, "");
+
+            background.setBackgroundColor(Color.parseColor(Colorchange));
+        }
+
+        if(sharedpreferences.contains(Mytheme))
+        {
+            Themechange = sharedpreferences.getInt(Mytheme,0);
+
+            background.setBackgroundResource(Themechange);
+        }
+
+
+    }
 }
